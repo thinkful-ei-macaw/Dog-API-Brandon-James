e945f80a354b707dd4e69f38eac3fd9c0201ef92
bleek42 <brandonleek42@gmail.com>
2020-02-20 14:04:36 -0500

    edited loop and get dog image

--- dog.js
+++ dog.js
@@ -1,34 +1,37 @@
-'use strict';
+//'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImage(input) {
+  if(input < 1 || input > 50 || input === NaN)
+  {
+    throw new TypeError('number must be 1 - 50');
+  } else {
+  let dogApi = `https://dog.ceo/api/breeds/image/random/${inputValue}`;
+  fetch(dogApi)
     .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
-}
-
-function randomDogImg(dogs){
-  for(let i = 0; i < dogs.length; i++){
-
-  }
+    .then(responseJson => displayResults(responseJson)) 
+    
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  //replace the existing image with the new one
   $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
+    `<img src="${responseJson.message}" class="results-img">`);
   $('.results').removeClass('hidden');
+}
+
+function generateDogs(num) {
+  let returnImg = 0;
+  for (let i = 0; i < num; i++){
+    returnImg += `<img src="${num[i]}" class="results-img">`
+  }
+  return returnImg;
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const num = $('#num').val();
-    getDogImage();
+    getDogImage(num);
   });
 }
 
